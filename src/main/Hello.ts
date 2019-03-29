8d5221d7207a6dadcb793fd196a3cf72213def15
IInfinity <DevinDon@Foxmail.com>
2019-03-29 23:35:53 +0800

    feat: add async method

--- src/main/Hello.ts
+++ src/main/Hello.ts
@@ -1,3 +1,7 @@
 export function hello(name: string): string {
   return `Hello, ${name}!`;
 }
+
+export async function asyncHello(name: string): Promise<string> {
+  return `Hello, ${name}!`;
+}
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,9 @@
-import { hello } from './Hello';
+import { hello } from './hello';
 import { logger } from '@iinfinity/logger';
 
 logger.info(hello('TypeScript template'));
+
+logger.debug('debug');
+logger.error('error');
+logger.info('info');
+logger.warn('warn');
